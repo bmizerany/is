63c28a4f3eddd1165db8a0af5667dedeee0eb71b
Blake Mizerany <blake.mizerany@gmail.com>
2022-08-18 15:24:33 -0700

    init

---
+++ is.go
@@ -0,0 +1,15 @@
+// Package is contains helpers for working with integers that might be even or
+// odd.
+package is
+
+import "golang.org/x/exp/constraints"
+
+// Even returns true if n can even.
+func Even[I constraints.Integer](n I) bool {
+	return n%2 == 0
+}
+
+// Odd returns false if n cannot even.
+func Odd[I constraints.Integer](n I) bool {
+	return !Even(n)
+}
